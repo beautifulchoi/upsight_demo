79b53f41bcbe2342ff894f271a4918a74c04e8cc
Eonji-sw <ejkim0625@gmail.com>
2023-07-25 17:12:28 +0900

    divide router

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:board_project/screens/auth_screen.dart';
-import 'package:board_project/screens/rounge/infinite_scroll_page.dart';
-import 'package:board_project/screens/tab_screen.dart';
+import 'package:board_project/router.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'constants/colors.dart';
 import 'firebase_options.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 
 void main() async {
   // calendar 한국어 설정을 위한 코드
@@ -28,21 +24,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'UpSight',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: KEY_BLUE),
         useMaterial3: true,
       ),
-      //initialRoute: '/',
-      initialRoute: '/auth',
-      //initialRoute: '/tab',
-      routes: {
-        //'/': (context) => BoardScreen(),
-        '/test': (context) => InfiniteScrollPage(),
-        '/auth': (context) => AuthScreen(),
-        '/tab': (context) => TabScreen(),
-      },
-      //home: BoardScreen(),
+      // Route를 관리하는 함수를 설정
+      onGenerateRoute: generateRoute,
+      initialRoute: authRoute, // 앱을 처음 시작할 때 표시할 첫 페이지 설정
     );
   }
 }
---
+++ lib/router.dart
@@ -0,0 +1,24 @@
+import 'package:board_project/screens/auth_screen.dart';
+import 'package:board_project/screens/rounge/infinite_scroll_page.dart';
+import 'package:board_project/screens/tab_screen.dart';
+import 'package:flutter/material.dart';
+
+// Route 이름 정의
+const String authRoute = '/';
+const String tabRoute = '/tab';
+const String boardRoute = '/tab/test';
+
+// Route를 관리하는 함수
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case authRoute:
+      return MaterialPageRoute(builder: (context) => AuthScreen());
+    case tabRoute:
+      return MaterialPageRoute(builder: (context) => TabScreen());
+    case boardRoute:
+      return MaterialPageRoute(builder: (context) => InfiniteScrollPage());
+    default:
+    // 잘못된 Route가 요청된 경우 로그인 페이지로 이동
+      return MaterialPageRoute(builder: (context) => AuthScreen());
+  }
+}
